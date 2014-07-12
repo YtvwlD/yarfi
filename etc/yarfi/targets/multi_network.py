3dac126141c6f4fbe8c6b27d3f285ca43128a021
Niklas Sombert <niklas@ytvwld.de>
2014-07-12 20:29:36 +0200

    Added a target for a multi user mode with network.

---
+++ etc/yarfi/targets/multi_network.py
@@ -0,0 +1,24 @@
+# YARFI - Yet Another Replacement For Init
+# Copyright (C) 2014 Niklas Sombert
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from yarfi.ServicesAndTargets import Target as Trg
+
+class Target(Trg):
+	def __init__(self):
+		self.description = "combination of multi and network"
+		self.depends_targets = ["multi", "network"]
+		self.depends_services = []
+		self.conflicts = []
