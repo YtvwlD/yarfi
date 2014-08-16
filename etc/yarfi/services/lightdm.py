12df1c909895cfe6bacdeec0a638981de227c89b
Niklas Sombert <niklas@ytvwld.de>
2014-08-16 17:00:41 +0200

    Added LightDM.

---
+++ etc/yarfi/services/lightdm.py
@@ -0,0 +1,41 @@
+# YARFI - Yet Another Replacement For Init
+# Copyright (C) 2014 Niklas Sombert
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from subprocess import Popen
+
+from yarfi.ServicesAndTargets import Service as Srv
+from yarfi.ServicesAndTargets import kill
+
+class Service(Srv):
+	def __init__(self):
+		self.description = "LightDM display manager"
+		self.depends = ["system", "dbus", "filesystem", "hostname"]
+		self.conflicts = []
+		self.respawn = True
+		self.process = None
+	
+	def start(self):
+		self.process = Popen(["/usr/sbin/lightdm"])
+	
+	def stop(self):
+		kill(self.process)
+	
+	def status(self):
+		if self.process:
+			if self.process.poll() is None:
+				return ("running")
+			else:
+				return ("stopped")
