e6d16f8d56aa8e1b2417f98bb24d7986ddd13a26
Niklas Sombert <niklas@ytvwld.de>
2014-08-31 16:57:49 +0200

    Wait for child processes to terminate to eliminate zombie processes. (similar to 85e8f1a)

--- usr/lib/yarfi/service.py
+++ usr/lib/yarfi/service.py
@@ -15,6 +15,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
+import os
 from PySide.QtCore import QCoreApplication, QTimer
 
 class YARFI:
@@ -22,10 +23,11 @@ class YARFI:
 		self.services = []
 		self.app = QCoreApplication(sys.argv)
 		self.debug = debug
-		self.respawnTimer = QTimer()
-		self.respawnTimer.timeout.connect(self.respawn)
-		self.respawnTimer.setInterval(30000) #is 30 seconds the right interval?
-		self.respawnTimer.start()
+		self.respawnAndZombieTimer = QTimer()
+		self.respawnAndZombieTimer.timeout.connect(self.respawn)
+		self.respawnAndZombieTimer.timeout.connect(self.zombie)
+		self.respawnAndZombieTimer.setInterval(30000) #is 30 seconds the right interval?
+		self.respawnAndZombieTimer.start()
 
 	def reach_target(self, wanted_target):
 		if self.debug:
@@ -44,13 +46,7 @@ class YARFI:
 		for dependency in remaining_dependencies:
 			self.start(dependency)
 		print(target.description + " target reached.")
-		sys.stdout.write("Eliminating zombie processes...")
-		sys.stdout.flush()
-		for service in self.services:
-			# TODO: If the service has died, this will return "stopped". What to do then?
-			service.status()
-		sys.stdout.write(" ...done.\n")
-		sys.stdout.flush()
+
 
 	def start(self, srv):
 		print ("Trying to start " + srv + "...")
@@ -102,6 +98,15 @@ class YARFI:
 				else:
 					print ("Removing it from the list of running services...")
 					self.services.remove(service)
+	
+	def zombie(self):
+		if self.debug:
+			print ("Eliminating zombie processes...")
+		try:
+			os.wait3(os.WNOHANG)
+		except OSError as e:
+			if e.errno != 10:
+				raise
 	
 	def exec_(self):
 		self.app.exec_()
